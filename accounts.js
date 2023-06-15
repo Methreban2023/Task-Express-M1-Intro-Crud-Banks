9c4ef855f40b71ee5c169b357294387d04332543
methreban2023 <alrefai.dalia@gmail.com>
2023-06-15 18:17:51 +0300

    commiting bank

--- accounts.js
+++ accounts.js
@@ -1,17 +1,23 @@
 const accounts = [
   {
     id: 1,
-    username: 'Omar',
+    username: "Omar",
+    email: "omar@hotmail.com",
+    accno: 1111111111,
     funds: 30,
   },
   {
     id: 2,
-    username: 'Zainab',
+    username: "Zainab",
+    email: "zainab@hotmail.com",
+    accno: 2222222222,
     funds: 0,
   },
   {
     id: 3,
-    username: 'Salwa',
+    username: "Salwa",
+    email: "salwa@hotmail.com",
+    accno: 3333333333,
     funds: 100,
   },
 ];
